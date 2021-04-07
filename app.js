7f548c8296558d03463bc887ddaf394182b2c614
Benjamin Krzis <benjamin@elisana.digital>
2021-04-07 17:35:10 +0200

    Add Eventlistener on button

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import { countries } from '/data.js'
 
 const list = document.querySelector('#countries')
+const buttonSubmit = document.querySelector(".submit")
+
+buttonSubmit.addEventListener("click", clicktest)
+
+function clicktest() {
+    event.preventDefault();
+    console.log("clikiiiii");
+}
 
 countries.forEach((country) => {
     const countryList = document.createElement('div')
     countryList.innerText = country.country;
     list.appendChild(countryList)
-    console.log(country.country)
+    // console.log(country.country)
 })
