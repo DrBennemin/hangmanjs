7b6b80490f7b8f286e1cfcd9efd4d9d14e8f211e
Benjamin Krzis <benjamin@elisana.digital>
2021-04-07 18:07:54 +0200

    Observe inut

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 import { countries } from '/data.js'
 
 const list = document.querySelector('#countries')
+const countryInput = document.querySelector(".input")
 const buttonSubmit = document.querySelector(".submit")
 
 buttonSubmit.addEventListener("click", clicktest)
 
 function clicktest() {
     event.preventDefault();
-    console.log("clikiiiii");
+    const newGuess = countryInput.value;
+    console.log(newGuess);
 }
 
 countries.forEach((country) => {
     const countryList = document.createElement('div')
     countryList.innerText = country.country;
     list.appendChild(countryList)
-    // console.log(country.country)
 })
